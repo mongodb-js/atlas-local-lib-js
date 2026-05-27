ebf8fb0ae25c8880c12944b4cf563ba588e0db5d
Jeroen Vervaeke <jeroen.vervaeke@mongodb.com>
2026-05-27 10:08:26 +0100

    feat(test): replace ava+nyc with vitest (CLOUDP-408325)

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -1,8 +1,8 @@
-import test from 'ava'
+import { test, expect } from 'vitest'
 
 import { Client } from '../index'
 
-test('smoke test', async (t) => {
+test('smoke test', async () => {
   let client: Client | null = null
 
   try {
@@ -11,53 +11,51 @@ test('smoke test', async (t) => {
   } catch (e: any) {
     // If docker is not running we get this error
     // any other error means failure
-    t.is(e?.message, 'connect to docker')
+    expect(e?.message).toBe('connect to docker')
     return
   }
 
   if (client == null) {
-    t.fail('Client not created')
-    return
+    throw new Error('Client not created')
   }
 
   // Skip test after client creation on Windows
   // Note all Windows return win32 including 64 bit
   if (process.platform === 'win32') {
-    t.pass('Skipping end-to-end test on Windows')
     return
   }
-  
+
   // Count initial deployments
   let start_deployments_count = (await client.listDeployments()).length
 
   // Create deployment
   let createDeploymentOptions = {
-    name: "test_deployment",
+    name: 'test_deployment',
     doNotTrack: true,
   }
   let deployment = await client.createDeployment(createDeploymentOptions)
-  t.is(deployment.name, createDeploymentOptions.name)
+  expect(deployment.name).toBe(createDeploymentOptions.name)
 
   // Get the deployment id twice, make sure it's the same
   let deploymentId = await client.getDeploymentId(createDeploymentOptions.name)
   let deploymentId2 = await client.getDeploymentId(createDeploymentOptions.name)
-  t.is(deploymentId, deploymentId2)
+  expect(deploymentId).toBe(deploymentId2)
 
   // Get deployment
   let getDeployment = await client.getDeployment(createDeploymentOptions.name)
-  t.is(getDeployment.name,createDeploymentOptions.name)
+  expect(getDeployment.name).toBe(createDeploymentOptions.name)
 
   let connString = await client.getConnectionString(createDeploymentOptions.name)
-  t.assert(connString === `mongodb://127.0.0.1:${getDeployment.portBindings.port}/?directConnection=true`)
+  expect(connString).toBe(`mongodb://127.0.0.1:${getDeployment.portBindings.port}/?directConnection=true`)
 
   // Count deployments after creation
   let after_create_deployment_count = (await client.listDeployments()).length
-  t.assert(after_create_deployment_count - start_deployments_count === 1)
+  expect(after_create_deployment_count - start_deployments_count).toBe(1)
 
   // Delete deployment
   await client.deleteDeployment(createDeploymentOptions.name)
-  
+
   // Count deployments after deletion
   let after_delete_deployment_count = (await client.listDeployments()).length
-  t.assert(start_deployments_count === after_delete_deployment_count)
+  expect(start_deployments_count).toBe(after_delete_deployment_count)
 })
